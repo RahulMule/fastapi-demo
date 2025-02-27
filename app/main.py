eaaa9f4408e4e0cfb773170be55417777df62531
RahulMule <rahulpmuleofficial@gmail.com>
2025-02-27 14:43:09 +0530

    adding logging using azure.monitor.opentelemetry

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+from azure.monitor.opentelemetry import configure_azure_monitor
+import logging
+import os
 app = FastAPI()
 
+
+conn_string = os.getenv("appinsightsKey")
+
+configure_azure_monitor(connection_string=conn_string)
+logger = logging.getLogger(__name__)
 games_db = [{
     "id": 1,
     "name": "Call of duty:warzone",
@@ -13,13 +20,16 @@ games_db = [{
     "category":"FPS"}]
 @app.get("/")
 async def read_root():
+    logger.info("received root request")
     return {"Hello": "World"}
 
 @app.get("/games")
 async def read_games():
+    logger.info("received games get request")   
     return games_db
 
 @app.post("/games")
 async def create_game(game: dict):
+    logger.info("received games post request with id as {game.id}")  
     games_db.append(game)
     return game
